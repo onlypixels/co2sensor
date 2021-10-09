e1e4958d2066629b0426984a88039429a3bd74e1
Ben Pitman <bmp103@gmail.com>
2021-10-09 21:08:35 +0100

    working prototype

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from smbus import SMBus
 import time
+import json
 
 # I2C simplified: 0x5E
 EE895ADDRESS = 0x5E
@@ -12,7 +13,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     i2cbus = SMBus(1)
-    return "<p>Hello, World!</p>"
     try:
         read_data = i2cbus.read_i2c_block_data(EE895ADDRESS, I2CREGISTER, 8)
         # read_data contains ints, which we need to convert to bytes and merge
@@ -24,24 +24,11 @@ def hello_world():
         resvd = read_data[4].to_bytes(1, 'big') + read_data[5].to_bytes(1, 'big')
         pressure = read_data[6].to_bytes(1, 'big') + read_data[7].to_bytes(1, 'big')
 
-        print("CO2: ", end='')
-        print(int.from_bytes(co2, "big"), end='')
-        print(" ppm [0x", end='')
-        print(co2.hex(), end='')
-        print("] | Temperature: ", end='')
-        print(int.from_bytes(temperature, "big") / 100, end='')
-        print("°C [0x", end='')
-        print(temperature.hex(), end='')
-        print("] | Reserved: ", end='')
-        print(int.from_bytes(resvd, "big"), end='')
-        print(" [0x", end='')
-        print(resvd.hex(), end='')
-        print("] | Pressure: ", end='')
-        print(int.from_bytes(pressure, "big") / 10, end='')
-        print("mbar [0x", end='')
-        print(pressure.hex(), end='')
-        print("]")
-        int.from_bytes(co2, "big")
+        response_dict = {
+            "co2_ppm": int.from_bytes(co2, "big"),
+            "temperature_c": int.from_bytes(temperature, "big") / 100,
+            "pressure_mbar": int.from_bytes(pressure, "big") / 10
+	}
+        return json.dumps(response_dict)
     except Exception:
-        print("Could not read from the sensor. Is it attached?")
-    
+        return json.dumps({"error": "cannot read sensor"})
